85ee1fdf5dcffcbdb0167666398d0cfb0b9494ba
trex-coder <110154601+trex-coder@users.noreply.github.com>
2025-05-16 11:59:07 +0530

    Update upload_data.py

--- firebase/upload_data.py
+++ firebase/upload_data.py
@@ -1,14 +1,17 @@
+# File: firebase/upload_data.py
 import requests
 import sys
 import json
 
-if len(sys.argv) != 4:
-    print("Usage: python upload_data.py <firebase_url> <key> <value>")
+FIREBASE_URL = "https://guacamole-a69bd-default-rtdb.asia-southeast1.firebasedatabase.app"
+
+if len(sys.argv) != 3:
+    print("Usage: python upload_data.py <key> <value>")
     sys.exit(1)
 
-firebase_url = sys.argv[1].rstrip('/')
-key = sys.argv[2]
-value = sys.argv[3]
+key = sys.argv[1]
+value = sys.argv[2]
 
-res = requests.put(f"{firebase_url}/{key}.json", data=json.dumps(value))
+res = requests.put(f"{FIREBASE_URL.rstrip('/')}/{key}.json", data=json.dumps(value))
 print("Upload result:", res.status_code, res.text)
+
